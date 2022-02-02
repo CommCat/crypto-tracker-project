3df7d83de9a983cfec51a19cfaec37660ae9a8cf
CommCat <commandantecat@gmail.com>
2022-02-02 19:05:08 +0100

    second commit

--- js/main.js
+++ js/main.js
@@ -5,3 +5,37 @@ document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+const llamandoApi = () => fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+
+llamandoApi()
+.then(function(data){
+  return data.json()
+})
+.then(function(data){
+  console.log(data.market_data.current_price.eur)
+  return data
+})
+.catch(function(err){
+  return err
+});
+
+
+/*const callApi = () => fetch("https://api.coingecko.com/api/v3/coins/bitcoin");
+
+const callApiAgain = () => {
+  const result = callApi()
+.then(function(data){
+  return data.json()
+})
+  console.log(result)
+  return result
+}
+
+callApiAgain()  
+.then(function(data){ console.log(data.market_data.current_price.eur)
+  return data
+})
+.catch(function(err){
+  return err
+});
+*/
