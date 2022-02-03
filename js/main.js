842864c1d41be0b1a4d896ee30d5b2f138e709f9
CommCat <commandantecat@gmail.com>
2022-02-03 19:43:59 +0100

    set currencies js finished

--- js/main.js
+++ js/main.js
@@ -25,19 +25,30 @@ const callApi = async () => {
 callApi()
 
 const divBitcoin = document.querySelector(".bitcoin");
-//console.log(divBitcoin)
 
-
-const getPriceAndShowInDom = async () => {
+const getBitcoinAndShowInDom = async () => {
   const bitcoinPrice = await callApi();
   const divBitcoinPrice = document.createElement("p");
   console.log(bitcoinPrice);
 
-  divBitcoinPrice.textContent = `current price: ${bitcoinPrice[0].current_price} €  ath price: ${ bitcoinPrice[0].ath} € ranking:  ${ bitcoinPrice[0].market_cap_rank}`;
+  divBitcoinPrice.textContent = `current price: ${bitcoinPrice[0].current_price}€  ath price: ${ bitcoinPrice[0].ath}€ ranking:  ${ bitcoinPrice[0].market_cap_rank}`;
 
   divBitcoin.appendChild(divBitcoinPrice);
 };
-getPriceAndShowInDom();
+getBitcoinAndShowInDom();
+
+
+const divEthereum = document.querySelector(".ethereum");
+
+const getEthereumAndShowInDom = async () => {
+  const ethereumPrice = await callApi();
+  const divEthereumPrice = document.createElement("p");
+  
+  divEthereumPrice.textContent = `current price: ${ethereumPrice[1].current_price}€  ath price: ${ ethereumPrice[1].ath}€ ranking:  ${ ethereumPrice[1].market_cap_rank}`;
+
+  divEthereum.appendChild(divEthereumPrice);
+};
+getEthereumAndShowInDom();
 
 
 
